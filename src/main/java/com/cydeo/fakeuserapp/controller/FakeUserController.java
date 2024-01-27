bc02aa7d38e451ce1cd93a08bda820f3693037b9
CundullahT-CT <cterzioglu@cybertekschool.com>
2024-01-27 16:10:44 +0100

    adds db

--- src/main/java/com/cydeo/fakeuserapp/controller/FakeUserController.java
+++ src/main/java/com/cydeo/fakeuserapp/controller/FakeUserController.java
@@ -3,6 +3,7 @@ package com.cydeo.fakeuserapp.controller;
 import com.cydeo.fakeuserapp.service.FakeUserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,11 @@ public class FakeUserController {
     @GetMapping("/no-db/random")
     public ResponseEntity<String> getRandomName() {
         return ResponseEntity.ok(fakeUserService.getRandomName());
+    }
+
+    @GetMapping("/with-db/random")
+    public ResponseEntity<String> getNameDromDB() {
+        return ResponseEntity.ok(fakeUserService.getNameFromDB());
     }
 
 }
--- src/main/java/com/cydeo/fakeuserapp/service/FakeUserService.java
+++ src/main/java/com/cydeo/fakeuserapp/service/FakeUserService.java
@@ -1,5 +1,9 @@
 package com.cydeo.fakeuserapp.service;
 
 public interface FakeUserService {
+
     String getRandomName();
+
+    String getNameFromDB();
+
 }
